ed68c9fa8861c6186a5e290c68c4165550c38823
juniorcarvalho <antoniomarcosdecj18@gmail.com>
2022-06-25 18:02:56 -0300

    enviando os arquivos do dia 8.3

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/parafixar.js
@@ -0,0 +1,22 @@
+// const sum = (num1, num2) => num1 + num2;
+// const sub = (num1, num2) => num1 - num2;
+// const multi = (num1, num2) => num1 * num2;
+// const div = (num1, num2) => num1 / num2;
+
+// const calculator = (fnctn) => fnctn(20, 5);
+
+// console.log(calculator(sum));
+
+const wakeup = () => 'Acordando!!';
+
+const timeForCoffee = () => 'Bora tomar café!!';
+
+const timeToSleep = () => 'Partiu dormir!!';
+
+// console.log(wakeup());
+// console.log(timeForCoffee());
+// console.log(timeToSleep());
+
+const doingThings = (fnctn) => fnctn()
+
+console.log(doingThings(wakeup));
