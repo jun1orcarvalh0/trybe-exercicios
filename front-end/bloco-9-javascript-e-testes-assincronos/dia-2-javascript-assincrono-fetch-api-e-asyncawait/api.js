a502c1ba9a55657f147957d2d27c1e3a06d112e2
Junior Carvalho <antoniomarcosdecj18@gmail.com>
2022-07-11 16:38:58 -0300

    fazendo os exercicios do dia 9.2 obs: falta o bonus

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -4,10 +4,20 @@ const fetchCoins = async () => {
   try {
     const response = await fetch(url)
     const data = await response.json();
-    console.log(data.data);
+    const coinsList = document.getElementById('coins-list');
+    data.data.filter((cryptocoin) => {
+      if (cryptocoin.rank <= 10) {
+        const eachCoin = document.createElement('li');
+        eachCoin.innerHTML = `${cryptocoin.name} (${cryptocoin.symbol}): ${cryptocoin.priceUsd}`;
+        coinsList.appendChild(eachCoin);
+      }
+    })
+    
   } catch(error) {
     console.log(`Algo deu errado :( \n${error}`)
   }
 }
 
-fetchCoins()
+window.onload = () => {
+  fetchCoins();
+}
