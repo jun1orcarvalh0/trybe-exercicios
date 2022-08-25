f7090fadabad31b3d796fb4420035ab4c44ca4af
Junior Carvalho <antoniomarcosdecj18@gmail.com>
2022-08-25 14:08:58 -0300

    implementando os ensinamentos da aula 15.5

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/doguinhos-app/src/App.test.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/doguinhos-app/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// src/App.test.js
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { waitFor, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import fetchMock from 'fetch-mock-jest';
+import App from './App';
+import renderWithRedux from './helper';
+
+describe('Página principal', () => {
+  test('Testa que o botão de adicionar cachorro está presente', async () => {
+    renderWithRedux(<App />);
+    const buttonDoguinho = screen.queryByText('Novo Doguinho');
+
+    expect(buttonDoguinho).toBeInTheDocument();
+
+    fetchMock.getOnce('https://dog.ceo/api/breeds/image/random', {
+      body: { message: 'myDogUrl' },
+    });
+
+    userEvent.click(buttonDoguinho);
+    await waitFor(() => expect(fetchMock.called()).toBeTruthy());
+  });
 });
